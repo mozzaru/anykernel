3d33aa73dfe00e5c974006b133b6fe58ed08c9bd
mozzaru <rianyanuar52@gmail.com>
2023-11-21 13:58:12 +0700

    AK3: Bringup for markw

--- anykernel.sh
+++ anykernel.sh
@@ -4,14 +4,14 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Fussion Kernel for Redmi note 4/4x mido by Alone0316 
+kernel.string=Kernel for Redmi 4 Prime markw
 do.devicecheck=1
 do.modules=0
 do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=mido
-supported.versions=9-14
+device.name1=markw
+supported.versions=10-14
 supported.patchlevels=2020-01-
 '; } # end properties
 
