5fc8197de4ca4a7167411e279f24d55183520393
mozzaru <rianyanuar52@gmail.com>
2025-04-10 14:29:46 +0700

    initial kernel-name: perf for markw

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=fluidKernel 4.9-337 for Redmi 4 Prime
+kernel.string=PerfKernel 4.9-337 for Redmi 4 Prime
 do.devicecheck=1
 do.modules=0
 do.systemless=0
